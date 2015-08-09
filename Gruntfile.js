13121dc2fea2aab440202e828b3c0ebecc87fda6
take-sei <sei06k14@gmail.com>
2015-08-10 03:16:03 +0900

    create local server

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,17 @@ module.exports = function(grunt) {
             }
         },
 
+        connect:{
+          server:{
+            options:{
+              port: 8000,  // 適当で可
+              keepalive: true,
+              hostname: 'localhost',
+              base: '.'
+            }
+          }
+        },
+
         watch: {
             javascript: {
               files: ['js/*.js'],
@@ -23,11 +34,15 @@ module.exports = function(grunt) {
 
     //loading the plague in (ex. grunt.loadNpmTasks('grunt-contrib-xxxx');
     grunt.loadNpmTasks("grunt-contrib-jshint");
-    grunt.loadNpmTasks("grunt-contrib-watch")
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-connect");
     //setting "Default tasks"
     grunt.registerTask("start", ["watch"]);
-    grunt.registerTask("pkg", ["pkg"]);
+    grunt.registerTask("s", ["connect"]);
     grunt.registerTask("default",function() {
-    grunt.log.writeln('だから、grunt start だってｗ');
-  })
+      grunt.log.writeln('だから、grunt start だってｗ\n 詳しくは grunt help');
+    });
+    grunt.registerTask("help",function() {
+      grunt.log.writeln('s (connect) =>local server \n start (watch) =>manage your js \n  ');
+    });
 }
